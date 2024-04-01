a8dfdc288aa53d10d13394dbff543c5d847305a7
Ivan Lopes <lopesivan.ufrj@gmail.com>
2024-04-01 12:41:20 -0300

    Atualiza o arquivo pythagoras/Makefile

--- pythagoras/etc/brew.hello.2.sh
+++ pythagoras/etc/brew.hello.2.sh
@@ -8,7 +8,7 @@ LOCAL_REPO=${REPO_NAME}
 
 echo '# Step 4 - Upload diff'
 
-hub clone my_patches
+#hub clone my_patches
 test -d my_patches/${REPO_NAME} || mkdir my_patches/${REPO_NAME}
 cp *.diff my_patches/${REPO_NAME}
 cd my_patches
