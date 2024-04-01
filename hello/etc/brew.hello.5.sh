13fb73e465b6a89625e0ef7272c7e9423de073ff
Ivan Lopes <lopesivan.ufrj@gmail.com>
2024-04-01 10:04:32 -0300

    Atualiza o arquivo hello/Makefile

--- hello/etc/brew.hello.5.sh
+++ hello/etc/brew.hello.5.sh
@@ -11,7 +11,7 @@ hub clone my_recipes
 cp ${MY_RECIPE} my_recipes/
 cd my_recipes
 git add .
-git ci -m "[${VERSION}] update ${MY_RECIPE}"
+git ci -m "[${VERSION}] new ${MY_RECIPE}"
 git push
 
 exit 0
