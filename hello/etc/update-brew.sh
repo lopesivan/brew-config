8942ca03fe689652b1304eec98dc1c727a7a8da8
Ivan Lopes <lopesivan.ufrj@gmail.com>
2024-04-01 11:28:19 -0300

    Atualiza o arquivo hello/Makefile

--- hello/etc/update-brew.sh
+++ hello/etc/update-brew.sh
@@ -4,6 +4,8 @@ OFF_FLAG=$(brew --prefix)/Homebrew/Library/Homebrew/shims/linux/super/.OFF
 
 if test -e $OFF_FLAG; then
 	echo "abortando: .OFF existe" >&2
+	echo faca:
+	echo make -C $(brew --prefix)
 	exit 42
 else
 	brew update
