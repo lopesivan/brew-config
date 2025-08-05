a74eda3db1886eba206166c56aa7fbba7a206987
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-08-05 08:19:55 -0300

    🎨 :style: Atualiza o estilo do arquivo st-autotools/install.rb

--- st-autotools/install.rb
+++ st-autotools/install.rb
@@ -1,9 +1,13 @@
   def install
-    # Força o uso do pkg-config do sistema
-    ENV["PKG_CONFIG"] = "/usr/bin/pkg-config"
 
-    # Força o path onde estão os .pc files
-    ENV["PKG_CONFIG_PATH"] = "/usr/lib/x86_64-linux-gnu/pkgconfig"
+    ENV["PKG_CONFIG_PATH"] = [
+    Formula["freetype"].opt_lib/"pkgconfig",
+    Formula["fontconfig"].opt_lib/"pkgconfig",
+    Formula["libpng"].opt_lib/"pkgconfig",
+    Formula["zlib"].opt_lib/"pkgconfig",
+    Formula["bzip2"].opt_lib/"pkgconfig",
+    Formula["expat"].opt_lib/"pkgconfig"
+    ].join(":")
 
     system "./gera-opcao.sh", "enable", "debug", '"Ativa modo de depuração"'
     system "sh", "autogen.sh"
