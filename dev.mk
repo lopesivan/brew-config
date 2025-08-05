e14bee1eeaafb3fad957dac65dc586391d7ba6e2
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-08-05 06:56:35 -0300

    🎨 :style: Atualiza o estilo do arquivo dev.mk

--- dev.mk
+++ dev.mk
@@ -3,15 +3,27 @@ HOMEBREWHOME = /home/linuxbrew/.linuxbrew
 LIBRARY      = $(HOMEBREWHOME)/Homebrew/Library
 SUPER        = $(LIBRARY)/Homebrew/shims/linux/super
 OFFFLAG      = $(SUPER)/.OFF
+MY_RECIPES   = my_recipes2
+MY_PATCHES   = my_patches
 
 all: update
 
-set:
-	@make -C /home/linuxbrew/.linuxbrew
-	@( test -e $(OFFFLAG) && echo existe: não faça update || echo não exite: pode fazer update )
+hub:
+	hub clone $(REPO)
 
 config: $(REPO)
 	@(cd $(REPO); bash ../etc/brew.config.sh $(REPO) $(VERSION) ) > data.repo
+
+init: hub config
+	@bold=$$(tput bold); \
+		bg_blue=$$(tput setab 4); \
+		bg_red=$$(tput setab 1); \
+		reset=$$(tput sgr0); \
+		echo "Realize as modificações em $$bold$$bg_blue $(REPO) $$reset e commite" ; \
+		echo "Ao final faça:$$bold$$bg_red make deploy $$reset"
+
+deploy: diff upload_diff new_recipes upload_recipes update
+original: version recipes upload_original update install
 
 version: recipe/$(RECIPE).rb
 	sed "s/version '.*'$$/version '$(VERSION)'/" -i recipe/$(RECIPE).rb
@@ -22,50 +34,70 @@ uninstall:
 	brew $@ $(RECIPE)
 
 clone:
-	@echo `tput bold tput setb 3`Clonando repositório `tput sgr0`
-	(source ./data.repo; git clone $${REMOTE_REPO})
-	@echo `tput bold tput setb 3`Agora realize as mudancas `tput sgr0`
+	@bold=$$(tput bold); \
+	bg_yellow=$$(tput setab 3); \
+	reset=$$(tput sgr0); \
+	echo "$$bold$$bg_yellow Clonando repositório $$reset"; \
+	. ./data.repo; \
+	git clone $$REMOTE_REPO; \
+	echo "$$bold$$bg_yellow Agora realize as mudancas $$reset"
 
-deploy: diff upload_diff new_recipes upload_recipes update
 
 diff: $(REPO)
-	@echo `tput bold tput setb 3`Cria o arquivo diff `tput sgr0`
+	@bold=$$(tput bold); \
+	bg_blue=$$(tput setab 4); \
+	reset=$$(tput sgr0); \
+	echo "$$bold$$bg_blue Cria o arquivo diff $$reset"; \
 	bash ./etc/brew.hello.1.sh
 
 upload_diff: patches
-	@echo `tput bold tput setb 3`Upload o arquivo diff `tput sgr0`
+	@bold=$$(tput bold); \
+	bg_blue=$$(tput setab 4); \
+	reset=$$(tput sgr0); \
+	echo "$$bold$$bg_blue Upload o arquivo diff $$reset"; \
 	bash ./etc/brew.hello.2.sh
 
 new_recipes:
-	@echo `tput bold tput setb 3`Cria novo recipes `tput sgr0`
+	@bold=$$(tput bold); \
+	bg_blue=$$(tput setab 4); \
+	reset=$$(tput sgr0); \
+	echo "$$bold$$bg_blue Cria novo recipes $$reset"; \
 	bash ./etc/brew.hello.3.sh
 
 upload_recipes: recipes
-	@echo `tput bold tput setb 3`Upload recipes `tput sgr0`
-	bash ./etc/brew.hello.4.sh
-	@echo `tput bold tput setb 4`brew update`tput sgr0`
-	@echo `tput bold tput setb 4`brew upgrade`tput sgr0`
+	@bold=$$(tput bold); \
+	bg_blue=$$(tput setab 4); \
+	reset=$$(tput sgr0); \
+	echo "$$bold$$bg_blue Upload recipes $$reset"; \
+	bash ./etc/brew.hello.4.sh; \
+	echo "$$bold$$bg_blue brew update $$reset"; \
+	echo "$$bold$$bg_blue brew upgrade $$reset"
 
 upload_original:
-	@echo `tput bold tput setb 3`Upload original recipes `tput sgr0`
-	bash ./etc/brew.hello.5.sh
-	@echo `tput bold tput setb 3`Não esqueça:`tput sgr0`
-	@echo `tput bold tput setb 4`brew update`tput sgr0`
-	@echo `tput bold tput setb 4`brew upgrade`tput sgr0`
+	@bold=$$(tput bold); \
+	bg_blue=$$(tput setab 4); \
+	reset=$$(tput sgr0); \
+	echo "$$bold$$bg_blue Upload original recipes $$reset"; \
+	bash ./etc/brew.hello.5.sh; \
+	echo "$$bold$$bg_blue Não esqueça: $$reset"; \
+	echo "$$bold$$bg_blue brew update $$reset"; \
+	echo "$$bold$$bg_blue brew upgrade $$reset"
 
-original: version recipes upload_original update install
 
 update:
-	bash ./etc/update-brew.sh
+	bash ./etc/$@-brew.sh
+upgrade:
+	bash ./etc/$@-brew.sh
 
 clean:
 	(source ./data.repo; rm -rf $${REPO_NAME})
 	rm -rf a b  *.diff
-	rm -rf my_patches my_recipes
+	rm -rf $(MY_PATCHES) $(MY_RECIPES) my_recipes
 	rm $(RECIPE).rb
 
 recipes:
-	hub clone my_recipes
+	hub clone $(MY_RECIPES)
+	mv $(MY_RECIPES) my_recipes
 
 patches:
-	hub clone my_patches
+	hub clone $(MY_PATCHES)
