9c62674bddaadc8d404d3c10426a8297fcfc7ff6
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-08-05 06:56:35 -0300

    🎨 :style: Atualiza o estilo do arquivo etc/update-brew.sh

--- etc/update-brew.sh
+++ etc/update-brew.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
 
-OFF_FLAG=$(brew --prefix)/Homebrew/Library/Homebrew/shims/linux/super/.OFF
-
-if test -e $OFF_FLAG; then
-	echo "abortando: .OFF existe" >&2
-	echo faca:
-	echo make -C $(brew --prefix)
-	exit 42
-else
-	brew update
-	brew upgrade
-fi
+brew update
