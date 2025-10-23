96c391ca9bd79414ce7c014f5bb03374f1bb692d
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-10-23 16:54:12 -0300

    :style: Atualiza o estilo do arquivo hw/install.rb

--- hw/install.rb
+++ hw/install.rb
@@ -1,4 +1,16 @@
-  def install
-    system 'cmake', '.'
-    system 'make', "PREFIX=#{prefix}", 'install'
+def install
+  args = %W[
+    --no-warn-unused-cli
+    -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE
+    -DCMAKE_BUILD_TYPE:STRING=Debug
+    -DCMAKE_C_COMPILER:FILEPATH=clang
+    -DCMAKE_CXX_COMPILER:FILEPATH=clang++
+    -DCMAKE_INSTALL_PREFIX=#{prefix}
+  ]
+
+  mkdir("build") do
+    system("cmake", *args, "..", "-G", "Ninja")
+    system("ninja")
+    system("ninja", "install")
   end
+end
